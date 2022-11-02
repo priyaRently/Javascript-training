4d03e70e00d3f0443b19a2113f4099eac1cdb089
Priya Kumari <priyajnv1999@gmail.com>
2022-11-02 18:04:57 +0530

    new file added

---
+++ async.js
@@ -0,0 +1,22 @@
+setTimeout (()=> {
+    console.log("Two")
+},3000)
+setTimeout (()=> {
+    console.log("One")
+},1000)
+setTimeout (()=> {
+    console.log("One")
+},1000)
+setTimeout (()=> {
+    console.log("Five")
+},0)
+setTimeout (()=> {
+    console.log("Three")
+},2000)
+setTimeout (()=> {
+    console.log("Three")
+},2000)
+setTimeout (()=> {
+    console.log("Four")
+},0)
+console.log("Zero")
